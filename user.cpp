f86cc5b0261c80216bb7d769897980c2371c371e
sheldoncross <sheldoncrosss@gmail.com>
2021-02-21 08:35:40 -0500

    Created user header file

--- user.cpp
+++ user.cpp
@@ -5,30 +5,31 @@
 
 #include <string> 
 #include <iostream>
+#include "user.h"
 
 using namespace std;
 
-class User {
-    //TODO: Create the account class for all bank accounts
-    //Used to store the accounts being accessed by the
-    //current user of the session
-    private: string* bankAccounts = new string[1]{"none"};
+//TODO: Create the account class for all bank accounts
+//Used to store the accounts being accessed by the
+//current user of the session
+void User::setBankAccounts(string* accounts) {
+    bankAccounts = accounts;
+}
 
-    //Returns the bank accounts belonging to the user
-    public: string* getBankAccounts() {
-        return bankAccounts;
-    }
-    
-    //TODO: Add a bank account to the current user instance (ROUGH version)
-    public: void addAccount(string account) {
-        int currentSize = sizeof(this->bankAccounts)/sizeof(this->bankAccounts) + 1;
-        string* newArray = new string[currentSize + 1];
-        for(int i = 0; i < currentSize-1; i++) {
-            newArray[i] = this->bankAccounts[i];
-        }
-        newArray[currentSize-1] = account;
-        bankAccounts = newArray;
-    }
+//Returns the bank accounts belonging to the user
+string* User::getBankAccounts() {
+    return bankAccounts;
+}
 
-    //addSessionTransaction is to be moved to BankSession
-};
+//TODO: Add a bank account to the current user instance (ROUGH version)
+void User::addAccount(string account) {
+    int currentSize = sizeof(this->bankAccounts)/sizeof(this->bankAccounts) + 1;
+    string* newArray = new string[currentSize + 1];
+    for(int i = 0; i < currentSize-1; i++) {
+        newArray[i] = this->bankAccounts[i];
+    }
+    newArray[currentSize-1] = account;
+    User::setBankAccounts(newArray);
+}
+
+//addSessionTransaction is to be moved to BankSession
---
+++ user.h
@@ -0,0 +1,18 @@
+#ifndef user_h
+#define user_h
+
+#include <string>
+
+using namespace std;
+
+class User {
+    private:
+        string* bankAccounts;
+
+    public:
+        string* getBankAccounts();
+        void addAccount(string account);
+        void setBankAccounts(string* accounts);
+};
+
+#endif
