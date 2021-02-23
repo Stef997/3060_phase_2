900de50510d637b30d6d29efc2a040a890daa30e
Anthony Huang <Huang.Anthony@outlook.com>
2021-02-23 14:19:25 -0500

    Added account attribute/method requirements, added a temporary hardcoded account constructor

--- account/account.h
+++ account/account.h
@@ -3,20 +3,25 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Account { 
     private:
         string number;
+        string name;
+        string status;
         float balance;
         string plan;
     public:
         Account();
+        Account(string bankAccountLine);
         string getNumber();
         void setNumber(string newNumber);
         float getBalance();
         void addBalance(float newBalance);
+        void removeBalance(float newBalance);
         string getPlan();
         void setPlan(string newPlan);
     protected:
